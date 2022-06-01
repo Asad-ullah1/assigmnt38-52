845fae9409dc10d43098dd6262ff9f732e314b20
Asad <52581107+Asad-ullah1@users.noreply.github.com>
2022-06-01 07:11:06 +0500

    first

---
+++ app.js
@@ -0,0 +1,145 @@
+ //  question1
+
+//  function power(a,b){
+//      document.write(Math.pow(a,b))
+//  }
+//  power(10,2);
+
+// question 2
+
+
+// function checkLeapYear(year) {
+
+    
+//     if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+//         document.write(year + ' is a leap year');
+//     } else {
+//         document.write(year + ' is not a leap year');
+//     }
+// }
+
+// // take input
+// const year = prompt('Enter a year:');
+
+// checkLeapYear(year);
+
+// question 3
+// var s1 = 2;
+// var s2 = 3;
+// var s3 = 4;
+// var s = (s1 + s2 + s3) / 2;
+// var area = Math.sqrt(s * ((s - s1) * (s - s2) * (s - s3)));
+// console.log(area);
+
+
+//  QUESTION 5
+
+
+// var a = "The quick brow fox jumps over the lazy dog"
+// var b = a.indexOf("x")
+
+// q 8
+
+// var distance = +prompt("Write the distence B/W two cities" + "in KM");
+
+// var b = distance * 1000; //for km to meter
+// var c = distance * 3280.84; // for km to feets
+// var d = distance * 39370.1 //for KM to inches
+// var e = distance * 100000 //for Km to centimeter
+// document.write(distance + " KM convert into meter is = " + b + "<br>" +
+//     distance + " KM convert into feet is = " + c + "<br>" +
+//     distance + " KM convert into inches is = " + d + "<br>" +
+//     distance + " KM convert into centimeter is = " + e + "<br>");
+
+/// ch 43-48
+// question 1
+
+
+// function clickme(){
+//     alert("it's working");
+// }
+
+// question 2
+// function mouseclick(){
+//     alert("Thanks for purchasing a phone from us")
+// }
+// question 3
+
+// function delte(){
+//     var get = document.getElementById("t1");
+//     var em = get.innerHTML = ""
+// }
+// function delte1(){
+//     var get = document.getElementById("t2");
+//     var em = get.innerHTML = ""
+// }
+// function delte2(){
+//     var get = document.getElementById("t3");
+//     var em = get.innerHTML = ""
+// }
+// function delte3(){
+//     var get = document.getElementById("t4");
+//     var em = get.innerHTML = ""
+// }
+//question 4
+// function onimouse(){
+//     var add=document.getElementById("imag");
+//     add.src="key.jpg";
+// }
+// function onimout(){
+//     var add=document.getElementById("imag");
+//     add.src="head.jpg";
+// }
+
+//wueston 5
+// function dec(){
+  
+//     var get = document.getElementById("myinput").value
+//     get--
+//     document.getElementById("myinput").value = get
+// }
+// function counter(){
+  
+//     var get = document.getElementById("myinput").value
+//     get++
+//     document.getElementById("myinput").value = get
+// }
+
+// ch 49-52
+// queston 1
+
+// function cool() {
+
+//     var get = document.getElementById("email").value;
+//     var pass = document.getElementById("pass").value;
+//     alert("Your email is " + get + '\n' + " Your Passord is " + pass);
+ 
+
+// }
+
+
+// question 2
+
+// function foo() {
+
+//     var fst = "You can read more about the box-sizing property in our CSS Box Sizing Tutorial. Footer. At";
+
+//     var get = document.getElementById("my");
+//     get.innerHTML += fst
+//     var link = document.getElementById("a1")
+//     link.style.display = "none"
+
+// }
+
+
+
+// function deleteRow() {
+//     var table = document.getElementById("dataTable").deleteRow(0);
+    
+// }
+// function edit(){
+//     var ad=document.getElementById("dataTable");
+// }
+
+   
+
